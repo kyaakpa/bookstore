ae4764ccda11b713c9bfbba690894ebc53f964c5
kyaakpa <kyaakpa@users.noreply.github.com>
2023-09-02 01:23:06 +1000

    spinner

--- frontend/src/components/Spinner.jsx
+++ frontend/src/components/Spinner.jsx
@@ -1,6 +1,8 @@
 const Spinner = () => {
   return (
-    <div className='animate-ping w-16 h-16 m-8 rounded-full bg-sky-400'></div>
+    <div className="flex justify-center">
+    <div className='animate-ping w-16 h-16 m-8 rounded-full bg-sky-400 mt-36'></div>
+    </div>
   )
 }
 
