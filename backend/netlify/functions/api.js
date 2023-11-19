efd64dbadb5afe67564f2cff5cb6657e832cb9a4
kyaakpa <100368545+kyaakpa@users.noreply.github.com>
2023-11-20 00:11:32 +1100

    api fix

--- backend/netlify/functions/api.js
+++ backend/netlify/functions/api.js
@@ -8,4 +8,18 @@ router.get("/hello", (req, res) => res.send("Hello World!"));
 
 api.use("/api/", router);
 
+api.use("/books", booksRoute);
+
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log("Server connected to Database");
+    app.listen(PORT, () => {
+      console.log(`Server running on port: ${PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
+
 export const handler = serverless(api);
